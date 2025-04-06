5de0a28ff6828aa8ef9fb323423dae026bdeee75
Igor <igorpracianoth@gmail.com>
2025-04-05 23:10:07 -0300

    Resto dos Models

---
+++ ApiAcademiaUnifor.ApiService/Models/WorkoutExercise.cs
@@ -0,0 +1,19 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace ApiAcademiaUnifor.ApiService.Models
+{
+    [Table("workout_exercises")]
+    public class WorkoutExercise : BaseModel
+    {
+        [PrimaryKey("id")]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("workout_id")]
+        public int WorkoutId { get; set; }
+
+        [Column("exercise_id")]
+        public int ExerciseId { get; set; }
+    }
+}
